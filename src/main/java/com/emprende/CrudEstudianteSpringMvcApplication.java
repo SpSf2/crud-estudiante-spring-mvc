cc3886e2ef548c1fc1f81f6cddbfea2ef6d8b6b7
SpSf2 <kiworks.duglas@gmail.com>
2026-06-16 13:07:40 +0200

    inicio de proyecto estudiante

---
+++ src/main/java/com/emprende/CrudEstudianteSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.emprende;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudEstudianteSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudEstudianteSpringMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emprende/CrudEstudianteSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emprende;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudEstudianteSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
